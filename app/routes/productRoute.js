4515882630a6f7bc54b505b6e9a791aa5316ca59
kenpeter <figo2476@hotmail.com>
2017-03-27 14:57:19 +1100

    updte

--- app/routes/productRoute.js
+++ app/routes/productRoute.js
@@ -2,6 +2,18 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
+  app.get('/product/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('products').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.get('/product/list', (req, res) => {
     var limit = req.query.limit;
 
